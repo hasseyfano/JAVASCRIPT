3efee1a91846458f27e18bdbef19e7b1f92540ff
hasseyfano <hasseymoi@gmail.com>
2024-05-06 16:25:57 -0400

    final

--- DOM/welcome/welcome.js
+++ DOM/welcome/welcome.js
@@ -1,6 +1,8 @@
 userName = document.getElementById("name");
 function myFunction() {
     document.getElementById("nameSubmitted").innerHTML = "Welcome " + userName.value;
-    window.open(url, '_blank');
+    h1.style.display = "none";
+    userName.style.display = "none";
+    btn.style.display = "none";
 
 }
